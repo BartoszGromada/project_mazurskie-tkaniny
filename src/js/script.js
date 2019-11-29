b0edce05f8239e9cf0c05ba17439b8f43b0b0b5e
Bartosz Gromada <bartosz.k.gromada@gmail.com>
2019-11-29 14:41:21 +0100

    upgrade_js_file

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,4 @@
-'use strict';
-
-const select = {
-  all: {
-    section: '.info',
-    activeSection: '.post.active',
-    activeLinks: '.menu_item a.active',
-  },
-  listOf: {
-    menuItem: '.menu_item a',
-  },
-  data: {
-    section: 'data-section',
-    background: 'data-background',
-  },
-  splash: '.splash_image',
-};
-
-const className = {
-  active: 'active',
-  deactive: 'deactive',
-  splash: {
-    one: 'splash_one',
-    two: 'splash_two',
-    three: 'splash_three',
-  }
-};
+import {select, className} from './settings.js';
 
 function activeClass(event) {
   event.preventDefault();
