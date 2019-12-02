8cec4372a161819497290676abbcd13f9cb3e6b6
Bartosz Gromada <bartosz.k.gromada@gmail.com>
2019-12-02 10:07:31 +0100

    finish_project

---
+++ dist/js/script.js
@@ -0,0 +1,78 @@
+/* eslint-disable linebreak-style */
+
+const select = {
+  all: {
+    section: '.info',
+    activeSection: '.post.active',
+    activeLinks: '.menu_item a.active',
+  },
+  listOf: {
+    menuItem: '.menu_item a',
+  },
+  data: {
+    section: 'data-section',
+    background: 'data-background',
+  },
+  splash: '.splash_image',
+};
+
+const className = {
+  active: 'active',
+  deactive: 'deactive',
+  splash: {
+    one: 'splash_one',
+    two: 'splash_two',
+    three: 'splash_three',
+  }
+};
+
+function addClickListenersToMenu() {
+  const menuItems = document.querySelectorAll(select.listOf.menuItem);
+  for (let item of menuItems) {
+    item.addEventListener('click', activeClass);
+  }
+}
+
+function activeClass(event) {
+  event.preventDefault();
+  const clickedElement = event.target;
+  const section = clickedElement.getAttribute(select.data.section);
+  const background = clickedElement.getAttribute(select.data.background);
+  const sectionArray = section.split(' ');
+  const activeSections = document.querySelectorAll(select.all.activeSection);
+  const activeLinks = document.querySelectorAll(select.all.activeLinks);
+  const splash = document.querySelector(select.splash);
+
+  for (let activeSection of activeSections) {
+    activeSection.classList.remove(className.active);
+    activeSection.classList.add(className.deactive);
+  }
+
+  for (let sectionItem of sectionArray) {
+    const sectionSelect = document.querySelectorAll('.' + sectionItem);
+    for (let select of sectionSelect) {
+      select.classList.remove(className.deactive);
+      select.classList.add(className.active);
+    }
+  }
+
+  for (let link of activeLinks) {
+    link.classList.remove(className.active);
+  }
+
+  clickedElement.classList.add(className.active);
+
+  if (background === className.splash.one) {
+    splash.classList.remove(className.splash.two, className.splash.three);
+  }
+  if (background === className.splash.two) {
+    splash.classList.remove(className.splash.one, className.splash.three);
+  }
+  if (background === className.splash.three) {
+    splash.classList.remove(className.splash.one, className.splash.two);
+  }
+
+  splash.classList.add(background);
+}
+
+addClickListenersToMenu();
